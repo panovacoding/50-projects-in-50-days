18d5cb17f8914494a219183eafc39c06f67e0fbe
Lidiya <lidakonstantinova@mail.ru>
2024-08-29 14:52:39 +0400

    feature: project 2 done

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,66 @@
 document.addEventListener('DOMContentLoaded', () => {
-    expandingCardsInit();
+	expandingCardsInit()
+	progressStepsInit()
 })
 
 const expandingCardsInit = () => {
-    const cards = document.querySelectorAll('.expand-card');
-    const clearActiveCards = () => {
-        cards.forEach((card) => card.classList.remove('expand-card_active'));
-    }
-    const changeActiveCard = (card) => (e) => {
-        clearActiveCards();
-        if(!card.classList.contains('expand-card_active')) {
-            card.classList.add('expand-card_active');
+	const cards = document.querySelectorAll('.expand-card')
+	const clearActiveCards = () => {
+		cards.forEach((card) => card.classList.remove('expand-card_active'))
+	}
+	const changeActiveCard = (card) => (e) => {
+		clearActiveCards()
+		if (!card.classList.contains('expand-card_active')) {
+			card.classList.add('expand-card_active')
+		}
+	}
+	cards.forEach((card) => {
+		card.addEventListener('click', changeActiveCard(card))
+	})
+}
+
+const progressStepsInit = () => {
+    const steps = document.querySelectorAll('.step');
+    const btnNext = document.getElementById("step-next");
+    const btnPrev = document.getElementById("step-prev");
+    const progressBar = document.querySelector(".steps-container__progress");
+    let activeStep = document.querySelector(".step_active");
+
+    const disableStepButtons = (progressBarWidth) => {
+        switch(progressBarWidth) {
+            case 0:
+                btnPrev.setAttribute("disabled", true);
+                break;
+            case 100: 
+                btnNext.setAttribute("disabled", true);
+                break;
+            default:
+                btnPrev.removeAttribute("disabled");
+                btnNext.removeAttribute("disabled");
         }
     }
-    cards.forEach((card) => {
-        card.addEventListener('click', changeActiveCard(card))
-    });
+
+    const updateProgressBarWidth = () => {
+        const activeSteps = document.querySelectorAll(".step_active");
+        const currentWidth = ((activeSteps.length - 1) / (steps.length - 1)) * 100;
+      
+        progressBar.style.width = `${currentWidth}%`;
+
+        disableStepButtons(currentWidth);
+    }
+
+    const btnNextClickHandler = () => {
+        activeStep = activeStep.nextElementSibling;
+        activeStep.classList.add("step_active");
+        updateProgressBarWidth();
+    }
+
+    const btnPrevClickHandler = () => {
+      activeStep.classList.remove("step_active");
+      activeStep = activeStep.previousElementSibling;
+      updateProgressBarWidth();
+    }
+    
+    btnNext.addEventListener("click", btnNextClickHandler);
+    btnPrev.addEventListener("click", btnPrevClickHandler);
 }
